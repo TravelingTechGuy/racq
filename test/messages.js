ec6b44280fe22b3b9b2864ff783caf9757a1493f
Guy Vider <guy@guyvider.com>
2014-06-07 22:40:16 -0700

    Process messages, and expose markers for prev/next requests

--- test/messages.js
+++ test/messages.js
@@ -9,10 +9,10 @@ describe('Messages operations', function() {
 	var config = require('./testConfig'),
 		tokenPath = __dirname + '/token.json',
 		queueName = 'demoQueue' + Math.floor(Math.random() * 9000 + 1000),
-		message = [{
+		message = {
 			ttl: 60,
 			body: {myProperty: 'myValue'}
-		}],
+		},
 		msgId = 'xxx';
 	
 	config.persistedTokenPath = tokenPath;
@@ -27,7 +27,7 @@ describe('Messages operations', function() {
 	});
 
 	it('should put message to queue ' + queueName, function(done) {
-		q.putMessages(queueName, message, done);
+		q.putMessages(queueName, [message], done);
 	});
 
 	it('should get messages from queue ' + queueName, function(done) {
@@ -35,10 +35,10 @@ describe('Messages operations', function() {
 			limit: 1,
 			echo: true
 		};
-		q.getMessages(queueName, options, function(error, messages) {
-			debug(util.inspect(messages, true, 3));
-			if(!error && messages.messages.length === 1 && messages.messages[0].body.myProperty === message[0].body.myProperty) {
-				msgId = messages.messages[0].href.split('/').pop();
+		q.getMessages(queueName, options, function(error, result) {
+			debug(util.inspect(result, true, 3));
+			if(!error && result.messages.length === 1 && result.messages[0].body.myProperty === message.body.myProperty) {
+				msgId = result.messages[0].id;
 				debug(msgId);
 				done();
 			}
@@ -47,9 +47,9 @@ describe('Messages operations', function() {
 
 	it('should get the message ' + msgId + ' from queue ' + queueName, function(done) {
 		debug(msgId);
-		q.getMessagesById(queueName, msgId, function(error, messages) {
-			debug(util.inspect(messages, true, 3));
-			if(!error && messages.body.myProperty === message[0].body.myProperty) {
+		q.getMessagesById(queueName, msgId, function(error, result) {
+			debug(util.inspect(result, true, 3));
+			if(!error && result.body.myProperty === message.body.myProperty) {
 				done();
 			}
 		});
